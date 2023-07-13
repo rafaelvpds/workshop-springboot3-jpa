009474c8a9f000c5fea29f0c308dfd7cc1bba8c9
rafaelvpds <33781893+rafaelvpds@users.noreply.github.com>
2023-07-13 15:34:57 -0300

    User update

--- src/main/java/com/rvweb/course/resources/UserResource.java
+++ src/main/java/com/rvweb/course/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,11 +48,18 @@ public class UserResource {
 		return ResponseEntity.created(uri).body(obj);
 	}
 
-	@DeleteMapping(value = "{id}")
+	@DeleteMapping(value = "/{id}")
 	// A resposta da requisição nao vai retornar nenhum corpo
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		userService.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = userService.updade(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
